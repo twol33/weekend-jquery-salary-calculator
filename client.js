666a46cb3c87e1c7a114ee15456c8070ff78ec06
Tou Xiong <tlxiong.prime@outlook.com>
2021-08-28 15:48:52 -0500

    allowed input fields to clear after input takes also added span to total monthly

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 $(document).ready(readyNow)
     console.log('JS ready');
     
+const totalCost = 20000;
 
 function readyNow() {
     $( '#submit-Button' ).on( 'click', addToTable )
@@ -8,6 +9,8 @@ function readyNow() {
 
     $( '#input-Info' ).on( 'click', '.remove-Button', deleteFromTable )
         console.log('remove clicker working');
+
+    let 
         
 }
 
@@ -30,6 +33,11 @@ function addToTable(){
             <td><button class="remove-Button">Remove</button></td>
         </tr>
     `);
+    $( '#employee-FirstName' ).val( '' );
+    $('#employee-LastName').val( '' );
+    $('#employee-Id').val( '' );
+    $('#employee-Title').val( '' );
+    $('#employee-Salary').val( '' );
 }
 // function addButtonToDOM() {
 //     $( '#remove-Button' ).on( 'click', deleteFromTable )
