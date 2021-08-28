024e58c5d12129e2a63a45fc7da8c0aa3e4db897
Tou Xiong <tlxiong.prime@outlook.com>
2021-08-28 13:39:45 -0500

    click added to submit and remove buttons

--- client.js
+++ client.js
@@ -3,5 +3,21 @@ $(document).ready(readyNow)
     
 
 function readyNow() {
+    $( '#submit-Button' ).on( 'click', addToTable )
+        console.log('submit clicker working');
+    
+
+    $( '#remove-Button' ).on( 'click', deleteFromTable )
+        console.log('remove clicker working');
+        
+}
+
+function addToTable(){
+    console.log('added to table');
+    
+}
+
+function deleteFromTable(params) {
+    console.log('removed from table');
     
 }
